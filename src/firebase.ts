931a07131834e63bf881868171875e4aeeff6dcc
Bong <borobong230@gmail.com>
2023-11-19 02:27:58 +0900

    feat: 환경파일 추가

--- src/firebase.ts
+++ src/firebase.ts
@@ -30,8 +30,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore();
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
 const auth = getAuth(app);
 const updateUserDB = (user: any) => {
   setDoc(doc(db, "users", user.email), {
